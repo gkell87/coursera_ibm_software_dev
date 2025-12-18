23212b44f54e81bc29b5d557baf2265785ef011c
gkell87 <g_kell87@yahoo.com>
2025-12-18 13:43:14 -0500

    finished test app

--- developing_ai_applications_with_python_and_flask/labs/test_mymodule.py
+++ developing_ai_applications_with_python_and_flask/labs/test_mymodule.py
@@ -1,6 +1,6 @@
 # Import unittest with the module script and functions
 import unittest
-from module import square, double
+from mymodule import square, double, add
 
 # define a test case class for testing
 class TestSquare(unittest.TestCase):
@@ -15,7 +15,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square(3.0), 9.0)
 
         # check that scalling square of -3 does not return -9
-        self.asserNotequal(square(-3), -9)
+        self.assertNotEqual(square(-3), -9)
 
 # define a test case for the double function
 class TestDouble(unittest.TestCase):
@@ -31,6 +31,27 @@ class TestDouble(unittest.TestCase):
 
         # check if properly computes 0
         self.assertEqual(double(0), 0)
+
+# define a test case for the add function
+class TestAdd(unittest.TestCase):
+
+    # define a test for the add function
+    def test1(self):
+
+        # check that calling a add of 2 and 4 gets 6
+        self.assertEqual(add(2, 4), 6)
+
+        # check that calling a add of 0 and 0 gets 0
+        self.assertEqual(add(0, 0), 0)
+
+        # check if calling a add of 2.3 and 3.6 gets 5.9
+        self.assertEqual(add(2.3, 3.6), 5.9)
+
+        # check if calling a add of Hello and World gets Hello World
+        self.assertEqual(add('Hello ', 'World'), 'Hello World')
+
+        # check if calling a add of -2 and -2 does not get 0
+        self.assertNotEqual(add(-2, -2), 0)
 
 # run all test cases
 unittest.main()
