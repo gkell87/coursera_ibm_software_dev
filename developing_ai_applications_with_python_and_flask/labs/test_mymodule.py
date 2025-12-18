5e9dc74cae3aec72562f2c8052f7f9275eb6b2b8
gkell87 <g_kell87@yahoo.com>
2025-12-18 13:07:19 -0500

    made updates

---
+++ developing_ai_applications_with_python_and_flask/labs/test_mymodule.py
@@ -0,0 +1,8 @@
+# Import unittest with the module script and functions
+import unittest
+from module import square, double
+
+# define a test case class for testing
+class TestSquare(unittest.TestCase):
+    # define test method for square function
+    def test1(self):
