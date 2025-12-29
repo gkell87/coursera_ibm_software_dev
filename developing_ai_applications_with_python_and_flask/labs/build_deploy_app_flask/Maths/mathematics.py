ec795206ac52a8003aef63691e9738240ebdb923
gkell87 <g_kell87@yahoo.com>
2025-12-29 18:01:25 +0000

    added new app framework

---
+++ developing_ai_applications_with_python_and_flask/labs/build_deploy_app_flask/Maths/mathematics.py
@@ -0,0 +1,12 @@
+# get sum
+def sum(a, b):
+    return a + b
+
+# get difference
+def diff(a, b):
+    return a - b
+
+# multiply
+def mult(a, b):
+    return a * b
+
---
+++ developing_ai_applications_with_python_and_flask/labs/build_deploy_app_flask/Maths/server.py
@@ -0,0 +1,4 @@
+from Maths.mathematics import sum, diff, mult
+from flask import Flask, render_template, request
+
+# for end-point /, implement a method that renders the index.html
