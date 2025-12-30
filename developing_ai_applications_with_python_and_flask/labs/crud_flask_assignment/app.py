d91e264109cfa63a4ad029f4325c2d961a33b10c
gkell87 <g_kell87@yahoo.com>
2025-12-30 12:29:04 -0500

    added GET and Post functions

--- developing_ai_applications_with_python_and_flask/labs/crud_flask_assignment/app.py
+++ developing_ai_applications_with_python_and_flask/labs/crud_flask_assignment/app.py
@@ -1,12 +1,39 @@
 # Import libraries
+from flask import Flask, redirect, request, render_template, url_for
 
 # Instantiate Flask functionality
+app = Flask('crud_app')
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
+@app.route('/')
+def get_transactions():
+    return render_template('transaction.html', transactions = transactions)
 
 # Create operation
+# Route handling GET and POST
+@app.route('/add' methods = ['GET', 'POST'])
+def add_transaction():
+    if request.method == 'POST':
+        transaction = {
+            'id': len(transactions) + 1,            # Generate a new ID based on the current length of the transactions list
+            'date': request.form['date'],           # Get the 'date' field value from the form
+            'amount': float(request.form['amount']) # Get the 'amount' field value from the form and convert it to a float
+        }
+        # append new transaction to the list
+        transactions.append(transaction)
+
+        # Redirect to the transactions list page after adding the new transaction
+        return redirect(url_for("get_transactions"))
+
+    # If the request method is GET, render the form template to display the add transaction form
+    return render_template("form.html")
 
 # Update operation
 
