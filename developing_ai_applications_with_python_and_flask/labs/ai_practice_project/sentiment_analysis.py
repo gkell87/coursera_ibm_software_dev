a825135f89aeb4d137391dc9d3fe7a3a234e6470
gkell87 <g_kell87@yahoo.com>
2025-12-31 11:35:17 -0500

    more fixed

--- developing_ai_applications_with_python_and_flask/labs/ai_practice_project/sentiment_analysis.py
+++ developing_ai_applications_with_python_and_flask/labs/ai_practice_project/sentiment_analysis.py
@@ -6,6 +6,6 @@ def sentiment_analyzer(text_to_analyse):
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     json = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json = json, header = header)
+    response = requests.post(url, json = json, header)
     return response.text
 
