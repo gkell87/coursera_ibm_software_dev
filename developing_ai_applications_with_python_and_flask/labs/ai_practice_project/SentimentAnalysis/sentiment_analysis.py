f8709d3d2be4cceeaa53356de4decfc41797e6ca
gkell87 <g_kell87@yahoo.com>
2025-12-31 13:52:20 -0500

    test

--- developing_ai_applications_with_python_and_flask/labs/ai_practice_project/SentimentAnalysis/sentiment_analysis.py
+++ developing_ai_applications_with_python_and_flask/labs/ai_practice_project/SentimentAnalysis/sentiment_analysis.py
@@ -1,13 +1,19 @@
-# Import the requests library to handle HTTP requests
-import json
 import requests
-
-# Define a function named sentiment_analyzer
-def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+import json
+def sentiment_analyzer(text_to_analyse):
+    # URL of the sentiment analysis service
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    # Constructing the request payload in the expected format
     myobj = { "raw_document": { "text": text_to_analyse } }
+    # Custom header specifying the model ID for the sentiment analysis service
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json = myobj, headers=header)
-    return {'label' : json.loads(response.text)['documentSentiment']['label'],
-            'score' : json.loads(response.text)['documentSentiment']['score']}
+    # Sending a POST request to the sentiment analysis API
+    response = requests.post(url, json=myobj, headers=header)
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    # Extracting sentiment label and score from the response
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+    # Returning a dictionary containing sentiment analysis results
+    return {'label': label, 'score': score}
 
