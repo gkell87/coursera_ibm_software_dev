18bb64cee0fd007df3f6d6de01feb3b78cf2cf51
gkell87 <g_kell87@yahoo.com>
2025-12-31 11:39:34 -0500

    revised

--- developing_ai_applications_with_python_and_flask/labs/ai_practice_project/sentiment_analysis.py
+++ developing_ai_applications_with_python_and_flask/labs/ai_practice_project/sentiment_analysis.py
@@ -2,10 +2,10 @@
 import requests
 
 # Define a function named sentiment_analyzer
-def sentiment_analyzer(text_to_analyse): 
+def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-    json = { "raw_document": { "text": text_to_analyse } }
-    headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json, headers)
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json = myobj, headers=header)
     return response.text
 
