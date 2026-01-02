e5ae3e2ec578e2ce47c32c2740740b8b773a6040
gkell87 <g_kell87@yahoo.com>
2026-01-02 14:35:58 -0500

    converted format

--- developing_ai_applications_with_python_and_flask/labs/final_project/emotion_detection.py
+++ developing_ai_applications_with_python_and_flask/labs/final_project/emotion_detection.py
@@ -6,8 +6,10 @@ import json, requests
 def emotion_detector(text_to_analyse):
     # Set Post Perameters
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
+               "content-type" : "json"}    
+                
     inputJson = { "raw_document": {"text": text_to_analyse }}
 
     # Return post response
-    return requests.post(url, json = inputJson, headers = headers).text
+    return requests.post(url, json = inputJson, headers = headers).json
