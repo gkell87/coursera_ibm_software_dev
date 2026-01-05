187ee3a743d7970a89e449ed6b6783935439cfd2
gkell87 <g_kell87@yahoo.com>
2026-01-05 12:18:51 -0500

    added server script

---
+++ developing_ai_applications_with_python_and_flask/labs/final_project/server.py
@@ -0,0 +1,27 @@
+# Import Flask, render_template, request
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Initiate the flask app
+app = Flask('Emotion Detector')
+
+# Assign route
+@app.route("\emotionDetector")
+def emo_analyzer():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass text to function in EmotionDetection Module
+    response = emotion_detector(text_to_analyze)
+
+    # Return a formatted string with the Emotion, Score and Dominant Emotion
+    return "For the given statement, the system response is".format(response.text) 'The dominant emotion is'.format(respone['dominant_emotion'])
+
+# Assign route to render the index.html in the template folder
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+# Assign host to run app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
