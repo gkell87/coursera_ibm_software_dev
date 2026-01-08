0053c4f7a42aec8de16accc72373b8916eecc310
gkell <g_kell87@yahoo.com>
2026-01-08 14:57:24 -0500

    fixed errors

--- django_application_developmenant_with_sql_and_databases/labs/exercise1_setting_up_first_django_app/orm/models.py
+++ django_application_developmenant_with_sql_and_databases/labs/exercise1_setting_up_first_django_app/orm/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Define your first model from here:
 class User(models.Model):
     # Charfield for users first name
-    first_name = models.CharField(null = False, max_lengh = 30, default = 'John')
+    first_name = models.CharField(null = False, max_length = 30, default = 'John')
     
     # Charfield for last name
     last_name =  models.CharField(null = False, max_length = 30, defualt = 'Doe')
