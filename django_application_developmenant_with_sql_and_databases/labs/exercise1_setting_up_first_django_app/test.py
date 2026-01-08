2a7044e81007a18f478b7a2a2097b2605df7bbe5
gkell <g_kell87@yahoo.com>
2026-01-08 15:07:42 -0500

    test

--- django_application_developmenant_with_sql_and_databases/labs/exercise1_setting_up_first_django_app/test.py
+++ django_application_developmenant_with_sql_and_databases/labs/exercise1_setting_up_first_django_app/test.py
@@ -1,16 +1,16 @@
 # Django specific settings
-import inspect, os
-os.environ.setdefault('DJANGO_SETTINGS_MODUEL', 'settings')
+import inspect
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from django.db import connection
-
 # Ensure settings are read
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# App specific imports
+# Your application specific imports
 from orm.models import User
 from datetime import date
- 
+
 # Delete all data
 def clean_data():
     User.objects.all().delete()
@@ -19,16 +19,16 @@ def clean_data():
 def test_setup():
     try:
         clean_data()
-        # Create test user and save
-        user = User(first_name = 'John', last_name = 'Doe', dob = date(1970, 3, 16))
+        # Create a test user and save to database
+        user = User(first_name='John', last_name='Doe', dob=date(1970, 3, 16))
         user.save()
         # Check user table is not empty
         assert User.objects.count() > 0
-        print('Model setup complete.')
+        print("Django Model setup completed.")
     except AssertionError as exception:
-        print('Setup fail with error: ')
-        raise(exceptoin)
+        print("Django Model setup failed with error: ")
+        raise(exception)
     except:
-        print('Unexpteced error')
-        
+        print("Unexpected error")
+
 test_setup()
