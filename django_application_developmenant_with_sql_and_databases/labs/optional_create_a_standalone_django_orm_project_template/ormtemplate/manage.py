1fad28673aea59dd68f80bf929eff4bb365f4466
gkell87 <g_kell87@yahoo.com>
2026-01-09 14:23:47 -0500

    made fixes

--- django_application_developmenant_with_sql_and_databases/labs/optional_create_a_standalone_django_orm_project_template/ormtemplate/manage.py
+++ django_application_developmenant_with_sql_and_databases/labs/optional_create_a_standalone_django_orm_project_template/ormtemplate/manage.py
@@ -1,8 +1,7 @@
 # import libraries
 import os, sys
-
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTING_MODULE', 'settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -10,7 +9,9 @@ if __name__ == '__main__':
             import django
         except ImportError:
             raise ImportError(
-                'Could not Import Django. Did you forget to activate Virtual Env?'
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
             )
         raise
     execute_from_command_line(sys.argv)
