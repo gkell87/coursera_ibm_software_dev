30329f0cc9e16041dc794c5806631c88bced82e3
gkell87 <g_kell87@yahoo.com>
2026-01-20 15:03:54 -0500

    revised code

--- django_application_developmenant_with_sql_and_databases/labs/exercise2_crud_template/write.py
+++ django_application_developmenant_with_sql_and_databases/labs/exercise2_crud_template/write.py
@@ -11,16 +11,17 @@ from crud.models import *
 from datetime import date
 
 
-# Your code starts from here:
 def write_instructors():
-    user_john = User(first_name = 'John', last_name = 'Doe', dob = date(1962, 7, 16))
+    # Add instructors
+    # Create a user
+    user_john = User(first_name='John', last_name='Doe', dob=date(1962, 7, 16))
     user_john.save()
-    instructor_john = Instructor(full_time = True, total_learners = 30050)
-    # Update user reference of instructor john
+    instructor_john = Instructor(full_time=True, total_learners=30050)
+    # Update the user reference of instructor_john to be user_john
     instructor_john.user = user_john
     instructor_john.save()
-
-    instructor_yan = Instructor(first_name = 'Yan', last_name = 'Lou', dob = date(1962, 7, 16), full_time = True, total_learners = 30050)
+    
+    instructor_yan = Instructor(first_name='Yan', last_name='Luo', dob=date(1962, 7, 16), full_time=True, total_learners=30050)
     instructor_yan.save()
     instructor_joy = Instructor(first_name='Joy', last_name='Li', dob=date(1992, 1, 2), full_time=False, total_learners=10040)
     instructor_joy.save()
@@ -28,30 +29,28 @@ def write_instructors():
     instructor_peter.save()
     print("Instructor objects all saved... ")
 
+
 def write_courses():
-    course_cloud_app = Course(name = 'Cloud app with Database',
-    description = 'Dev and deploy on cloulde')
-
+    # Add Courses
+    course_cloud_app = Course(name="Cloud Application Development with Database",
+                              description="Develop and deploy application on cloud")
     course_cloud_app.save()
-
-    course_python = Course(name = 'Intro to Python',
-    description = 'Learn core concepts of python')
-
+    course_python = Course(name="Introduction to Python",
+                           description="Learn core concepts of Python and obtain hands-on "
+                                       "experience via a capstone project")
     course_python.save()
-
-    print('Course Objects Saved...')
+    print("Course objects all saved... ")
 
 def write_lessons():
-    # Add Lessons
-    lesson_1 = Lesson(title = 'Lesson 1', content = 'Object Mapping')
-    lesson_1.save()
-
-    lesson_2 = Lesson(title = 'Lesson 2', content = 'Django')
-    lesson_2.save()
-    print('lessons saved...')
+    # Add lessons
+    lession1 = Lesson(title='Lesson 1', content="Object-relational mapping project")
+    lession1.save()
+    lession2 = Lesson(title='Lesson 2', content="Django full stack project")
+    lession2.save()
+    print("Lesson objects all saved... ")
 
 def clean_data():
-    # delete all and start from fresh
+    # Delete all data to start from fresh
     Enrollment.objects.all().delete()
     User.objects.all().delete()
     Learner.objects.all().delete()
@@ -59,3 +58,8 @@ def clean_data():
     Course.objects.all().delete()
     Lesson.objects.all().delete()
 
+# Clean any existing data first
+clean_data()
+write_courses()
+write_instructors()
+write_lessons()
