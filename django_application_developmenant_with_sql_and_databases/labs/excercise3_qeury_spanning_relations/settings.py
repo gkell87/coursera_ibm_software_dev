7612a2f6c119da0479c112bc5d3c21f344d6dd18
gkell87 <g_kell87@yahoo.com>
2026-01-26 10:02:28 -0500

    migrated data

---
+++ django_application_developmenant_with_sql_and_databases/labs/excercise3_qeury_spanning_relations/settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '8BYY63AeKAkLOLd8dgIrnPIW',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'related_objects',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
