d8d4e54b050f1855c5867cee2296c870479cf0b5
gkell87 <g_kell87@yahoo.com>
2026-01-26 11:25:18 -0500

    test

--- django_application_developmenant_with_sql_and_databases/labs/excercise3_qeury_spanning_relations/read_enrollments.py
+++ django_application_developmenant_with_sql_and_databases/labs/excercise3_qeury_spanning_relations/read_enrollments.py
@@ -1,3 +1,15 @@
+# Django specific settings
+import inspect
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.db import connection
+# Ensure settings are read
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+from related_objects.models import *
+from datetime import date
+
 learner_david = Learner.objects.get(first_name="David")
 print("1. Get the user information about learner `David`")
 print(learner_david.user_ptr)
