1ad83a47785ac96873f3e3f1e22781c617543ab0
gkell87 <g_kell87@yahoo.com>
2026-01-26 17:12:35 -0500

    finished test

--- django_application_developmenant_with_sql_and_databases/labs/excercise4_first_app_with_django/firstapp/urls.py
+++ django_application_developmenant_with_sql_and_databases/labs/excercise4_first_app_with_django/firstapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # Create a path object defining the URL pattern to the index view
-    path(route='', view=views.index, name='index'),
+    path(route = '', view = views.index, name = 'index'),
 
-    path(route='', view=views.get_data, name='date')
+    path(route  ='date', view = views.get_date, name = 'date')
 ]
