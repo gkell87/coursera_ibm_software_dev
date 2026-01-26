5c9d7114a54336dc5f833183a08db9c403da31e2
gkell87 <g_kell87@yahoo.com>
2026-01-26 17:04:52 -0500

    test

--- django_application_developmenant_with_sql_and_databases/labs/excercise4_first_app_with_django/firstapp/views.py
+++ django_application_developmenant_with_sql_and_databases/labs/excercise4_first_app_with_django/firstapp/views.py
@@ -1,12 +1,21 @@
 # import libraries from django
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def get_daate(request):
     # create a simple html page as a string
     template = '<html>' \
     'this is your first view' \
     '</html>'
+
+    return HttpResponse(content = template)
+
+def get_date(request):
+    today = date.today()
+    template = "<html>" \
+    "today's data {} " \
+    "</html>".format(today)
 
     return HttpResponse(content = template)
