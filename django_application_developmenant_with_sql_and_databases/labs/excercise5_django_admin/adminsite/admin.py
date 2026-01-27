1241b9d0f805d7164f28fe638b5e624bf93e2a84
gkell87 <g_kell87@yahoo.com>
2026-01-27 11:22:39 -0500

    added inline adding

--- django_application_developmenant_with_sql_and_databases/labs/excercise5_django_admin/adminsite/admin.py
+++ django_application_developmenant_with_sql_and_databases/labs/excercise5_django_admin/adminsite/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Course, Instructor, Lesson
 
-# Register your models here.
+# customize site
+class LessonInline(admin.StackedInline):
+    model = Lesson 
+    extra = 5
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+    inlines = [LessonInline]
+
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user', 'full_time']
+
+# update the registration
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Instructor, InstructorAdmin)
