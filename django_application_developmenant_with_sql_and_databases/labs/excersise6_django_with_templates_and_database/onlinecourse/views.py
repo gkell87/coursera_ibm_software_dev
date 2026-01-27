b1cee779fd792bee0fd67b168ff7e62e9710ecc9
gkell87 <g_kell87@yahoo.com>
2026-01-27 15:18:05 -0500

    added more features

--- django_application_developmenant_with_sql_and_databases/labs/excersise6_django_with_templates_and_database/onlinecourse/views.py
+++ django_application_developmenant_with_sql_and_databases/labs/excersise6_django_with_templates_and_database/onlinecourse/views.py
@@ -17,3 +17,17 @@ def popular_course_list(request):
         # Appen the course list as an entry of context dict
         context['course_list'] = course_list
         return render(request, 'onlinecourse/course_list.html', context)
+
+def enroll(request, couse_id):
+    # If the request method is POST
+    if request.method == 'POST':
+        # First try to read the course object
+        # If could be found, raise a 404 exception
+        course = get_object_or_404(Course, pk = course_id)
+        # Increase the enrollment by 1
+        course.total_enrollment +- 1
+        course.save()
+        # Return a HTTP response
+        return HttpResponseRedirect(reverse(viewname = 'onlinecourse:popular_course_list'))
+
+        
